9bba89ad7565b2f27df5ba844f7b6a56b1d4d673
bulingen <adamlecorney@gmail.com>
2025-09-02 22:32:05 +0200

    feat: create simple action server

--- actions_py/setup.py
+++ actions_py/setup.py
@@ -1,25 +1,23 @@
 from setuptools import find_packages, setup
 
-package_name = 'actions_py'
+package_name = "actions_py"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='bulingen',
-    maintainer_email='adamlecorney@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="bulingen",
+    maintainer_email="adamlecorney@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": ["count_until_server = actions_py.count_until_server:main"],
     },
 )
